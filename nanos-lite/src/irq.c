fe122a06d50724b9c18b88cd5229ef9e8e8f7d82
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-05-07 18:33:51 +0800

    finish hello

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,7 +7,6 @@ static Context* do_event(Event e, Context* c) {
       break;
     }
     case EVENT_SYSCALL:{
-      Log("System call...");
       do_syscall(c);
       break;
     }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,6 +5,9 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
     case SYS_yield:{
@@ -13,7 +16,21 @@ void do_syscall(Context *c) {
       break;
     }
     case SYS_exit:{
-      halt(0);
+      halt(a[1]);
+      break;
+    }
+    case SYS_write:{
+      int fd = a[1];
+      const char *buf = (const char *)a[2];
+      size_t len = a[3];
+      if (fd == 1 || fd == 2) {
+        for (size_t i = 0; i < len; i++) {
+          putch(buf[i]);
+        }
+        c->GPRx = len;
+      } else {
+        c->GPRx = -1;
+      }
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
